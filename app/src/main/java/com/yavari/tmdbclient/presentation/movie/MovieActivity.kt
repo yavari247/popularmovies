dacfa2f45115746800cf85252ff365f592a7471b
yavari247 <nafisehaliyavari@gmail.com>
2022-11-22 17:00:21 +0330

    add project to git

--- app/src/main/java/com/yavari/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/yavari/tmdbclient/presentation/movie/MovieActivity.kt
@@ -3,6 +3,9 @@ package com.yavari.tmdbclient.presentation.movie
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -53,6 +56,37 @@ class MovieActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext,"No data available",Toast.LENGTH_LONG).show()
             }
         })
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update->{
+                updateMovies()
+                true
+            }else ->
+                super.onOptionsItemSelected(item)
+            }
+    }
+    private fun updateMovies(){
+        binding.movieProgressBar.visibility=View.VISIBLE
+        val response=movieViewModel.updateMovies()
+        response.observe(this,Observer{
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility=View.GONE
+            }
+            else{
+                binding.movieProgressBar.visibility=View.GONE
+
+            }
+        })
+
     }
 
 }
